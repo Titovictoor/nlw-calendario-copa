02b387f0becc41b95fc27a51fdccb33d31c23489
Victor Fonseca Pessoa <titovictoortfm@gmail.com>
2022-12-06 21:32:31 -0300

    Quartas

--- oitavas.js
+++ oitavas.js
@@ -1,3 +1,4 @@
+/* ********************** FUNCTION QUE REALIZA O CLICK DO MENU RESPONSIVO **************** */
 const burguerInput = document.getElementById("burguer-input")
 
 function toggleMenu() {
@@ -6,15 +7,31 @@ function toggleMenu() {
 }
 
 burguerInput.addEventListener("click", toggleMenu)
-
-function createGame(player1, hour, player2) {
+/* ***********************FUNCTION QUE CRIA O JOGO ******************* */
+function createGame(player1, date, score, player2) {
+  const contentToDisplay = score ? score : date
+  const winner = calculateWinner(score)
   return `
     <li>
-      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
-      <strong>${hour}</strong>
-      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+      <img ${winner === "Player2" && 'class="loser"'} src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+      <strong>${contentToDisplay}</strong>
+      <img ${winner === "Player1" && 'class="loser"'} src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
     </li>
   `
+}
+/* ***************** FUNCTION DEFINE VENCEDOR **************** */
+function calculateWinner(score) {
+  if (!score) {
+    return ""
+  }
+  const [scoreP1, _, scoreP2] = score.split(" ")
+  if (Number(scoreP1) > Number(scoreP2)) {
+    return "Player1"
+  }
+  if (Number(scoreP1) < Number(scoreP2)) {
+    return "Player2"
+  }
+  return "empate"
 }
 
 let delay = -0.1
@@ -29,7 +46,7 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("03/12", "sábado", createGame("netherlands", "12:00", "eua") + createGame("argentina", "16:00", "australia")) +
-  createCard("04/12", "domingo", createGame("france", "12:00", "poland") + createGame("england", "16:00", "senegal")) +
-  createCard("05/12", "segunda", createGame("japan", "12:00", "croacia") + createGame("brazil", "16:00", "southkorea")) +
-  createCard("06/12", "terça", createGame("spain", "12:00", "marrocos") + createGame("portugal", "16:00", "switzerland"))
+  createCard("03/12", "sábado", createGame("netherlands", "12:00", "3 x 1", "eua") + createGame("argentina", "16:00", "2 x 1", "australia")) +
+  createCard("04/12", "domingo", createGame("france", "12:00", "3 x 1", "poland") + createGame("england", "16:00", "3 x 0", "senegal")) +
+  createCard("05/12", "segunda", createGame("japan", "12:00", "1 x 1", "croacia") + createGame("brazil", "16:00", "4 x 1", "southkorea")) +
+  createCard("06/12", "terça", createGame("spain", "12:00", "", "marrocos") + createGame("portugal", "16:00", "6 x 1", "switzerland"))
--- quartas.js
+++ quartas.js
@@ -7,15 +7,32 @@ function toggleMenu() {
 
 burguerInput.addEventListener("click", toggleMenu)
 
-function createGame(player1, hour, player2) {
+function createGame(player1, date, score, player2) {
+  const contentToDisplay = score ? score : date
+  const winner = calculateWinner(score)
   return `
     <li>
-      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
-      <strong>${hour}</strong>
-      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+      <img ${winner === "Player2" && 'class="loser"'} src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+      <strong>${contentToDisplay}</strong>
+      <img ${winner === "Player1" && 'class="loser"'} src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
     </li>
   `
 }
+
+function calculateWinner(score) {
+  if (!score) {
+    return ""
+  }
+  const [scoreP1, _, scoreP2] = score.split(" ")
+  if (Number(scoreP1) > Number(scoreP2)) {
+    return "Player1"
+  }
+  if (Number(scoreP1) < Number(scoreP2)) {
+    return "Player2"
+  }
+  return "empate"
+}
+
 let delay = -0.1
 function createCard(date, day, games) {
   delay = delay + 0.1
@@ -28,5 +45,5 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("09/12", "sexta", createGame("bola", "12:00", "bola") + createGame("bola", "16:00", "bola")) +
-  createCard("10/12", "sábado", createGame("bola", "12:00", "bola") + createGame("bola", "16:00", "bola"))
+  createCard("09/12", "sexta", createGame("brazil", "12:00", "", "croacia") + createGame("netherlands", "16:00", "", "argentina")) +
+  createCard("10/12", "sábado", createGame("portugal", "12:00", "", "marrocos") + createGame("england", "16:00", "", "france"))
